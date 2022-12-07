48dc5bb533fec332a76b38fa54c4a7018dec47f3
EmanuelBorrajero <emanuelborrajero@gmail.com>
2022-12-07 09:23:57 -0500

    Creacion de las bases de el proyecto

---
+++ root/app.js
@@ -0,0 +1,15 @@
+import bodyParser from 'body-parser'
+import http from 'http'
+
+const app = express()
+const server=http.createServer(app)
+const port = 8000
+app.use(bodyParser.json({extended:true}))
+//const {sequelize, Sequelize} = db
+//Middlewares
+
+//Routing
+
+//Error HandIng
+
+server.listen({port})
