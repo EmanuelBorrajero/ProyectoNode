b941a87f237d8c00bdf8b5dc763bb424f1bba37b
EmanuelBorrajero <emanuelborrajero@gmail.com>
2022-12-07 10:10:02 -0500

    Sigo creando la estructura de el proyecto

--- root/util/dataBaseControl.js
+++ root/util/dataBaseControl.js
@@ -1,1 +1,15 @@
 import {Sequelize, DataTypes} from 'sequelize'
+import modelDefinitions from '../models/index.js'
+const sequelize = new Sequelize({
+    host:'',
+    database:'',
+    username:'',
+    password:'',
+    dialect:''
+})
+const {models}=sequelize
+modelDefinitions.forEach(model=>model(sequelize, DataTypes, models))
+//Associations
+const{}=models
+//Associations
+export default {models, sequelize}
