d16ad2a98183f8ce9b122ed3e2bb4643b910cc0e
EmanuelBorrajero <emanuelborrajero@gmail.com>
2022-12-07 17:31:31 -0500

    configuracion para usar mi database

--- root/util/dataBaseControl.js
+++ root/util/dataBaseControl.js
@@ -1,11 +1,11 @@
 import {Sequelize, DataTypes} from 'sequelize'
 import modelDefinitions from '../models/index.js'
 const sequelize = new Sequelize({
-    host:'127.0.0.1:3306',
-    database:'databasemysql',
-    username:'Emanuel',
-    password:'2509',
-    dialect:'mysql',
+    host:'127.0.0.1:3307',
+    database:'databaseMDB',
+    username:'root',
+    password:'emanuel',
+    dialect:'mariadb',
     define:{freezeTableName:true}
 })
 const {models}=sequelize
